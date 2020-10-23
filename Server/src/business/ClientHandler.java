ddfd9caa572ced18739c2dc5619c6f94876b3a5d
loidinhm31 <loidinh.git@gmail.com>
2020-10-23 12:16:40 +0700

    Get rid of the unused import

--- Server/src/business/ClientHandler.java
+++ Server/src/business/ClientHandler.java
@@ -3,9 +3,7 @@ package business;
 import com.entity.Client;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 import controller.ServerBoxController;
