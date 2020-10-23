f44ca87f20c9f0d2964de3a1e1e0748e31db48ec
loidinhm31 <loidinh.git@gmail.com>
2020-10-23 12:21:45 +0700

    Get rid of the unused import

--- Client/src/com/ClientChatController.java
+++ Client/src/com/ClientChatController.java
@@ -5,7 +5,6 @@ import com.entity.Client;
 import com.entity.Server;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
